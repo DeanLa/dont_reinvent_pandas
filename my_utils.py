634e43ea5bf1b4ffac5b01f212660d8400470e51
DeanLa <DeanLa@gmail.com>
2018-12-16 17:50:54 +0200

    changes for PyData

--- my_utils.py
+++ my_utils.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 def make_sliding_time_windows(df, steps_back, col_name='mean_event'):
     ret = df.copy()
     for i in range(1, steps_back + 1):
-        ret['{}_{}'.format(col_name, i)] = ret[col_name].shift(i)
+        ret['{}-{}'.format(col_name, i)] = ret[col_name].shift(i)
     return ret
 
 
